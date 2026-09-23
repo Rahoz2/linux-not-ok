8ba29727ae4f05915888252aac10e71ef267f1d7
Rahoz2 <rahozumed96@gmail.com>
2026-09-23 22:24:07 +0300

    websocket world has been added hope it works

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const http= require('http');
+const http = require("http");
 
-http.createServer(function(req,res){
-res.write('On the way to being a full stack engineer');
-res.end();
-}).listen(3000);
+http
+  .createServer(function (req, res) {
+    res.write("On the way to being a full stack engineer");
+    res.end();
+  })
+  .listen(3000);
 
 console.log("We're listining to you through port 3000");
-
-
-
